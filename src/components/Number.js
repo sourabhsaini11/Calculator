f47ef95eb5c90ba7ccd3867b15100a890818cdf2
sourabhsaini11 <ss1841620@gmail.com>
2021-05-04 13:10:01 +0530

    Basic Design

---
+++ src/components/Number.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Number = ({ val, inputHandler }) => {
+    const numHandler = () => {
+        return (!isNaN(val) || val==="." || val==='=')
+    }
+    return(
+        <Button onClick={()=>inputHandler} correct={numHandler()}>
+        <h1>{val}</h1>
+        </Button>
+    );
+}
+
+const Button=styled.div`
+    height:5rem;
+    width:5rem;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+   // background-color:#e0e1e6;
+   background: ${prop => prop.correct ? '#e0e1e6' : '#fe9241'};
+   color: ${prop => prop.correct ? '#888888' : '#ffffff'};
+    //border:1px solid  #888888;
+   outline: 1px solid #888888;
+`;
+
+export default Number;
